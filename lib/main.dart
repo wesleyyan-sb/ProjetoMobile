b180aee2c0a20e3267df55dbbe040a723fe62503
Wesley Yan <wesleyy.soaresbrehmer@gmail.com>
2026-05-13 15:51:31 -0300

    Adicionar searchBar (acima do título) | apenas visual

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -50,8 +57,15 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
   child: Column(
-    mainAxisAlignment: MainAxisAlignment.start,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
+      TextField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          labelText: 'Buscar unidade...',
+          prefixIcon: Icon(Icons.search),
+        ),
+      ),
       Text('EstacionaAí', style: Theme.of(context).textTheme.displayLarge),
       const Text('Bem vindo! Crie uma conta ou faça login para continuar.'),
       ElevatedButton(onPressed: () {
@@ -63,13 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
   ),
 );
       }, child: Text("Criar conta")),
-      Divider(
-        height: 20,
-        thickness: 2,
-        indent: 10,
-        endIndent: 10,
-        color: Colors.white,
-      ),
+      const SizedBox(height: 16),
       ElevatedButton(onPressed: () {
 
 
@@ -80,6 +88,13 @@ class _MyHomePageState extends State<MyHomePage> {
   ),
 );
       }, child: Text("Fazer login")),
+            Divider(
+        height: 20,
+        thickness: 2,
+        indent: 10,
+        endIndent: 10,
+        color: Colors.white,
+      ),
       const Text('Nossas unidades:'),
       
       Expanded(
@@ -99,7 +114,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+        
       ),
+      const Text('A EstacionaAí é uma empresa fundada em Rio do Sul, que disponibiliza estacionamentos nas principais cidades do Brasil com eficiencia e responsabilidade.'),
     ],
     
   ),
